ba44ca40d1e677dd724d4ea2549d63f8cbc22a98
Samba <rocklee.no21@gmail.com>
2019-02-25 19:25:01 +0530

    this is my axios instance file. commit 1

---
+++ src/axios-orders.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+import axios from 'axios';
+
+const instance = axios.create({
+    baseURL: 'https://reactmyburgerbuilder.firebaseio.com/'
+});
+
+export default instance;
