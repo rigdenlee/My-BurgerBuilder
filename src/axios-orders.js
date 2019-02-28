bec1994ec5599de5aadfd535b661c41a80680c28
Samba <rocklee.no21@gmail.com>
2019-02-28 11:09:44 +0530

    overall files needed for burgerbuilder to update orders

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
-import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://reactmyburgerbuilder.firebaseio.com/'
+    baseURL: 'https://reactmyburgerbuilder.firebaseio.com'
 });
 
 export default instance;
